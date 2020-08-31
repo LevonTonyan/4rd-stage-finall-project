cbd86fbe4d3f9c85fb9fa937e03c7695e5423f0e
LevonTonyan <levontonyan87@gmail.com>
2020-08-31 11:01:16 +0400

    First commit

---
+++ conftest.py
@@ -0,0 +1,36 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: en, ru, fr or es")
+
+
+
+options = Options()
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    browser = None
+    if language == "es":
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    elif language == "ru":
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    elif language == "fr":
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    elif language == "en":
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    else:
+        raise pytest.UsageError("--page language should be en, fr, ru or es")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
